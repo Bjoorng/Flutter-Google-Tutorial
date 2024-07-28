964f777e45da566f434acf4de917cac8fd021812
luca.sernicola <luca.sernicola@gmail.com>
2024-07-28 18:22:40 +0000

    second page to app

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Counter app'),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Counter app'),
+        routes: {
+          '/second': (context) => const SecondPage(
+                title: 'To-Do List',
+          )
+        }
     );
   }
 }
@@ -27,10 +32,73 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key, required this.title});
+  final String title;
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  String input = '';
+  final List<String> _list = List.empty(growable: true);
+
+  void _addToList() {
+    setState(() {
+      _list.add(input);
+      input = '';
+    });
+  }
+
+  void _deleteFromList(String input) {
+    setState(() {
+      _list.removeWhere((element) => element == input);
+    });
+  }
+
+  void _clearList() {
+    setState(() {
+      _list.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Add items to your To-Do List',
+              ),
+              TextField(
+                onChanged: (value) {
+                  input = value;
+                },
+              ),
+              ElevatedButton(
+                onPressed: _addToList,
+                child: const Text('Add'),
+              ),
+              const Text(
+                'Your To-Do List:',
+              ),
+              Text(_list.toString()),
+            ]),
+      ),
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _every10 = 0;
-  List<int> _memory = List.empty(growable: true);
+  final List<int> _memory = List.empty(growable: true);
 
   void _incrementCounter() {
     setState(() {
@@ -52,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _emptyList(){
+  void _emptyList() {
     setState(() {
       _counter = 0;
       _every10 = 0;
@@ -66,6 +134,14 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/second');
+            },
+            icon: const Icon(Icons.apps_sharp),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
@@ -81,7 +157,8 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text('Try as many times as You want:'),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium, selectionColor: Colors.blue,
+              style: Theme.of(context).textTheme.headlineMedium,
+              selectionColor: Colors.blue,
             ),
           ],
         ),
